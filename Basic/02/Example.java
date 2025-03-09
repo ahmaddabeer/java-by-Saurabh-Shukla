5ea3288c2bbfc8ea4433c87761df634b7cb8e27c
Your Name <ahmaddabeer36@gmail.com>
2025-03-09 11:11:34 +0530

    difference b/t staic variable and isntant variable

---
+++ Basic/02/Example.java
@@ -0,0 +1,17 @@
+public class Example 
+{
+    int x;// Instance variable
+    static int y;// static member of variable
+    public void fun1(){}// Instance mamber function
+    public static void fun2(){}//static member function
+    // public void fun3(){static int a;} error ha isme 
+
+    //is class ke andar ak or ak or static class bana sakte ha
+
+    static class Test 
+    {
+
+    }//ye example ha static inner class ka 
+
+}
+   
