d52acfd1ab95522b065e6dac749db5002bb5bc45
Your Name <ahmaddabeer36@gmail.com>
2025-03-10 09:09:08 +0530

    use wrapper class valueOf()

---
+++ Basic/04Wrapper class in java/Example.java
@@ -0,0 +1,11 @@
+public class Example{
+
+    public static void main(String[] args)
+    {
+
+        Integer i1 = Integer.valueOf("333");
+       double i2 =  Double.valueOf("333.43");
+       System.out.println(i2);
+       System.out.println(i1);
+    }
+}
