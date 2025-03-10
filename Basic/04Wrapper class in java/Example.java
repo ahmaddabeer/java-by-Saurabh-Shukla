84c63fe307a3ba82a35c305359c52e398725ae55
Your Name <ahmaddabeer36@gmail.com>
2025-03-10 14:22:37 +0530

    parsennn ka example ha ye

--- Basic/04Wrapper class in java/Example.java
+++ Basic/04Wrapper class in java/Example.java
@@ -4,8 +4,8 @@ public class Example{
     {
 
         Integer i1 = Integer.valueOf("333");
-       double i2 =  Double.valueOf("333.43");
-       System.out.println(i2);
+       Double i2 =  Double.valueOf("333.43");
        System.out.println(i1);
+       System.out.println(i2);
     }
-}
+} 
---
+++ Basic/04Wrapper class in java/Example2.java
@@ -0,0 +1,10 @@
+public class Example2 {
+
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt("123");
+        double b = Double.parseDouble("3.14");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
