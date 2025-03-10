047f17062bcc4ef0e7b0b2b1f2cde8fe1b1d4a70
Your Name <ahmaddabeer36@gmail.com>
2025-03-10 08:32:58 +0530

    add new concept

---
+++ Basic/03/helo.java
@@ -0,0 +1,20 @@
+class Example
+{
+
+    int x;
+
+    static class Test {
+        public static String  a = "Er.Dabeer Ahmad";
+
+    }
+
+}
+
+public class helo
+{
+
+    public static void main(String[] arg){
+
+        System.out.println(Example.Test.a);
+    }
+}
