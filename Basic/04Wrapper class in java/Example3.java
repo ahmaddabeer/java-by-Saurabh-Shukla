a5923a26b9a05ed2f00b97d4df400104b9709c2e
Your Name <ahmaddabeer36@gmail.com>
2025-03-10 14:33:20 +0530

    use xxxvalue()function in java

---
+++ Basic/04Wrapper class in java/Example3.java
@@ -0,0 +1,12 @@
+
+
+public class Example3 {
+    
+    public static void main(String[] args){
+
+        Integer i1= Integer.valueOf("101011",2);
+
+        int c = i1.intValue();
+        System.out.println(c);
+    }
+}
