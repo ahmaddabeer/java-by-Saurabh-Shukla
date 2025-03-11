007397c22048ce7924d5b2a49ab3728c9f770095
Your Name <ahmaddabeer36@gmail.com>
2025-03-11 15:21:16 +0530

    use of Acces modifiers

---
+++ Basic/06AccessModifiers/Example.java
@@ -0,0 +1,21 @@
+
+public class Example {//outer class isme 2 option ha ye to public ya default
+
+    class Dummy //inner class isme use any of these public,private,protected,detault
+    {
+        public int a;
+
+    }
+    
+}
+
+
+
+/*
+public classs Example2
+{
+
+
+}
+
+ *///we know that in one java file only one java public class .
