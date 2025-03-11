1855c904107efaba2d510bda053f14c7789d1ad7
Your Name <ahmaddabeer36@gmail.com>
2025-03-11 17:09:17 +0530

    use of extends

---
+++ Basic/08Inheritance in java/Example.java
@@ -0,0 +1,18 @@
+
+public class Example {
+
+    public static void main(String []args){
+
+        Student s1 = new Student();
+        s1.setRollno(100);
+        s1.setName("kabir");
+        s1.setAge(18);
+
+        System.out.println("Rollno : "+s1.getRollno());
+        System.out.println("Name :"+s1.getName());
+        System.out.println("Age : "+s1.getAge());
+
+
+    }
+    
+}
