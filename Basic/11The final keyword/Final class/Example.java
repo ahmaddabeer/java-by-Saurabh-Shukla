6c30444b9f63163188a42d43fca88a056065b728
Your Name <ahmaddabeer36@gmail.com>
2025-03-14 01:04:26 +0530

    add new concept

---
+++ Basic/11The final keyword/Final class/Example.java
@@ -0,0 +1,5 @@
+final class Example {//this is called final class in java not change in this class 
+
+
+    
+}
--- Basic/11The final keyword/FonalLocalVaribale/Example.java
+++ Basic/11The final keyword/FonalLocalVaribale/Example.java
@@ -3,6 +3,6 @@ public class Example {
     public void fun(){
 
         final  int k;//final local variable
-        
+
     }
 }
