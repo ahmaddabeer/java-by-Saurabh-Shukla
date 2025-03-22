3b8509d588c4f8d4f1a40044b763b987bc10bad3
Your Name <ahmaddabeer36@gmail.com>
2025-03-23 00:15:02 +0530

    you know you do not create a abstract class object but create abstract class reference variable object

---
+++ Basic/19Abstract class in java/neabs.java
@@ -0,0 +1,20 @@
+//you do not create abstract class  object but you create abstract reference variable object 😂😂
+
+abstract class a{
+
+
+}
+
+class b extends a{
+
+
+}
+
+public class neabs
+{
+
+    public static void main(String []args){
+
+        a obj = new b();
+    }
+}
