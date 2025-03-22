14a8feb211bdd4c4cb83ca5be56bc8b22d011e9d
Your Name <ahmaddabeer36@gmail.com>
2025-03-23 00:52:08 +0530

    using abstract method

---
+++ Basic/20AbstractMethod in java/abs.java
@@ -0,0 +1,29 @@
+//what is wrong whth the code ?
+public class abs {
+    
+}
+
+abstract class person 
+{
+
+    abstract void show();
+
+}
+
+abstract class  student extends person{
+
+
+}
+
+
+class AbstractExample3
+{
+
+    public static void main(String []args){
+
+        // student s = new student();
+        
+        //error because can't be make an object of abstract class 
+        //student is also a abstract class because abstract method in student class that resun student class make a abstract class 
+       }
+}
