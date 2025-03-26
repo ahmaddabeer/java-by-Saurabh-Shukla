3c6220c92c4b5cddcde7c3a12e3965e749ddac97
Your Name <ahmaddabeer36@gmail.com>
2025-03-26 15:23:38 +0530

    2d array initialize

---
+++ Basic/242d array/ar.java
@@ -0,0 +1,9 @@
+public class ar {
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[4][5];
+
+        System.out.println("a[0]ki lenght "+arr[4].length);
+    }
+}
---
+++ Basic/242d array/ar2.java
@@ -0,0 +1,18 @@
+public class ar2 {
+    
+
+    public static void main(String[]args){
+
+        int [][]arr = new int [][] {{3,4},{5,6},{7,8}};
+
+        //print array
+
+        for(int i=0; i<3;i++){
+
+            for(int j=0; j<2; j++){
+
+                System.out.println(+arr[i][j]);
+            }
+        }
+    }
+}
