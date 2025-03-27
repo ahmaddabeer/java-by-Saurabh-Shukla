e43d2edc6affc89ae1cacf78aa49a71c1b61f602
Your Name <ahmaddabeer36@gmail.com>
2025-03-27 10:30:34 +0530

    add output

--- Basic/24 2d array/35 java/st.java
+++ Basic/24 2d array/35 java/st.java
@@ -7,10 +7,10 @@ class StringExample1
         String s2 = "computer";
         String s3 =new String("computer");
 
-        System.out.println("Result 1:"+(s1 == s2));
-        System.out.println("Result 2:"+s1.equals(s2));
-        System.out.println("Result 3:"+(s1 == s3));
-        System.out.println("Result 4:"+s1.equals(s3));
+        System.out.println("Result 1:"+(s1 == s2));//true
+        System.out.println("Result 2:"+s1.equals(s2));//true
+        System.out.println("Result 3:"+(s1 == s3));//false
+        System.out.println("Result 4:"+s1.equals(s3));//true
     }
 
 }
