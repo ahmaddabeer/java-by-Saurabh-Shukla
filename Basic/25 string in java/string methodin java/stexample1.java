49c23de1738a110798d23a3f19f0605fe72523f1
Your Name <ahmaddabeer36@gmail.com>
2025-03-28 23:39:41 +0530

    add a neew code

---
+++ Basic/25 string in java/string methodin java/stexample1.java
@@ -0,0 +1,10 @@
+public class stexample1 {
+
+    public static void main(String[] args) {
+
+        String s1 = new String("Computer");
+        System.out.println(s1);
+        String s2 = s1.toUpperCase();
+        System.out.println(s2);
+    }
+}
